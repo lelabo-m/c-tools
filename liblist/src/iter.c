eab4f98fa3228f3e0709671d328c4321199b7e69
lelabo_m <marc.lelabourier@epitech.eu>
2014-06-17 10:33:17 +0200

    liblist : change the end method and the IncCurrent/DecCurrentMethod, it's simple to iterate through a list

--- liblist/src/iter.c
+++ liblist/src/iter.c
@@ -5,7 +5,7 @@
 ** Login   <lelabo_m@epitech.net>
 **
 ** Started on  Mon Mar 10 15:00:42 2014 Marc Le Labourier
-** Last update Mon Mar 10 15:10:22 2014 Marc Le Labourier
+** Last update Tue Jun 17 10:31:43 2014 Marc Le Labourier
 */
 
 #include <stdlib.h>
@@ -22,7 +22,7 @@ t_node	*EndList(t_list *list)
 {
   if (!list)
     return (NULL);
-  return (list->_tail);
+  return (list->_tail->next);
 }
 
 t_node	*CurrentList(t_list *list)
@@ -36,7 +36,7 @@ void	IncCurrentList(t_list *list)
 {
   if (!list || list->is_empty(list))
     return ;
-  if (list->_current && list->_current->next)
+  if (list->_current)
     {
       list->_current = list->_current->next;
       list->_index += 1;
@@ -47,7 +47,7 @@ void	DecCurrentList(t_list *list)
 {
   if (!list || list->is_empty(list))
     return ;
-  if (list->_current && list->_current->prev)
+  if (list->_current)
     {
       list->_current = list->_current->prev;
       list->_index -= 1;
